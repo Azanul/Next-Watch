8071bea44ede81a4384fc58ed1aa536c61accd36
Azan <azanulhaque@gmail.com>
2021-08-18 13:07:49 +0530

    search functions implemented

--- db/db.go
+++ db/db.go
@@ -4,16 +4,18 @@ import (
 	"context"
 	"fmt"
 	"github.com/Azanul/Next-Watch/graph/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"time"
 )
 
 var mongoHost = "Azan:<password>@moviecluster.t9pn0.mongodb.net/movieDB"
 
 type DB struct {
-	client *mongo.Client
+	client *mongo.Collection
 }
 
 func Connect() *DB {
@@ -24,13 +26,16 @@ func Connect() *DB {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println("error connecting to mongoDB client: " + err.Error())
+	}
 	return &DB{
-		client: client,
+		client: client.Database("movieDB").Collection("movies"),
 	}
 }
 
 func (*DB) Save(movie *model.NewMovie) *model.Movie {
-	collection := Connect().client.Database("movieDB").Collection("movies")
+	collection := Connect().client
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	res, err := collection.InsertOne(ctx, movie)
@@ -50,4 +55,32 @@ func (*DB) Save(movie *model.NewMovie) *model.Movie {
 		Actors:  inputActors,
 		Watched: movie.Watched,
 	}
+}
+
+func (db DB) GetMovies(skill string) ([]*model.Movie, error) {
+	res, err := db.client.Find(context.TODO(), db.filter(skill))
+	if err != nil {
+		log.Printf("Error while fetching programmers: %s", err.Error())
+		return nil, err
+	}
+	var p []*model.Movie
+	err = res.All(context.TODO(), &p)
+	if err != nil {
+		log.Printf("Error while decoding programmers: %s", err.Error())
+		return nil, err
+	}
+	return p, nil
+}
+
+func (db DB) filter(attr string) bson.D {
+	return bson.D{{
+		"attr.name",
+		bson.D{{
+			"$regex",
+			"^" + attr + ".*$",
+		}, {
+			"$options",
+			"i",
+		}},
+	}}
 }
