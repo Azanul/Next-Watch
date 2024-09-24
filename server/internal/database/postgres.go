61cde1eb196856aeab038f4dda7bf20583e20171
Azanul <azanulhaque@gmail.com>
2024-09-24 13:22:56 +0530

    feat: db connection
    Signed-off-by: Azanul <azanulhaque@gmail.com>

---
+++ server/internal/database/postgres.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() *sql.DB {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	return db
+}
