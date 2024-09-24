2d642d58f3204d0bf3da3e1780035b738f1ce85b
Azanul <azanulhaque@gmail.com>
2024-09-25 04:51:33 +0530

    feat: movies component update
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- frontend/components/MainComponent.tsx
+++ frontend/components/MainComponent.tsx
@@ -13,22 +13,20 @@ export default function MainComponent() {
                 <div className='inline-flex rounded-md shadow-sm' role="group">
                     <button
                         type="button"
-                        className={`px-4 py-2 text-sm font-medium rounded-l-lg ${
-                            activeTab === 'movies'
-                                ? 'bg-sky-500 text-white'
-                                : 'bg-white text-gray-700 hover:bg-gray-50'
-                        } border border-gray-200`}
+                        className={`px-4 py-2 text-sm font-medium rounded-l-lg ${activeTab === 'movies'
+                            ? 'bg-sky-500 text-white'
+                            : 'bg-white text-gray-700 hover:bg-gray-50'
+                            } border border-gray-200`}
                         onClick={() => setActiveTab('movies')}
                     >
                         Movies
                     </button>
                     <button
                         type="button"
-                        className={`px-4 py-2 text-sm font-medium rounded-r-lg ${
-                            activeTab === 'recommendations'
-                                ? 'bg-blue-500 text-white'
-                                : 'bg-white text-gray-700 hover:bg-gray-50'
-                        } border border-gray-200`}
+                        className={`px-4 py-2 text-sm font-medium rounded-r-lg ${activeTab === 'recommendations'
+                            ? 'bg-blue-500 text-white'
+                            : 'bg-white text-gray-700 hover:bg-gray-50'
+                            } border border-gray-200`}
                         onClick={() => setActiveTab('recommendations')}
                     >
                         Recommendations
