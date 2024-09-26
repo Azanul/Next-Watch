84bc1c3bb28c445aa34f1802af317e1ef89861be
Azanul <azanulhaque@gmail.com>
2024-09-26 19:47:30 +0530

    feat: formbricks integration
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,11 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <FormbricksProvider />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-sky-400 to-sky-200`}
       >
         <ApolloWrapper>{children}</ApolloWrapper>
+        <FormbricksProvider />
       </body>
     </html>
   );
