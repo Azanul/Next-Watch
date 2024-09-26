035ecf6c2ac69b29d1b1923f6ab50e0d38561ab0
Azanul <azanulhaque@gmail.com>
2024-09-26 15:47:46 +0530

    fix: support float score in DB
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- server/db/migration/000003_add_ratings_table.up.sql
+++ server/db/migration/000003_add_ratings_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE ratings (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES users(id),
     movie_id UUID REFERENCES movies(id),
-    score INTEGER NOT NULL,
+    score REAL NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
